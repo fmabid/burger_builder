c68d2231e3890345fa1e76e17a7da81208150dcf
abid <fmaabid@gmail.com>
2018-10-15 20:35:21 +0600

    Overrite

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,26 +6,24 @@ import classes from './SideDrawer.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux/Aux';
 
-const sideDrawer = (props) => {
-  let attachedClasses = [classes.SideDrawer, classes.Close];
-  if (props.open) {
-    attachedClasses = [classes.SideDrawer, classes.Open];
-  }
-
-  return (
-    <Aux>
-      <Backdrop show={props.open} clicked={props.closed}/>
-
-      <div className={attachedClasses.join(' ')}>
-        <div className={classes.Logo}>
-          <Logo/>
-        </div>
-        <nav>
-          <NavigationItems/>
-        </nav>
-      </div>
-    </Aux>
-  );
+const sideDrawer = ( props ) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
+    );
 };
 
 export default sideDrawer;
