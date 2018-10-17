dd036f6129b7b26450f2b3b4e15734524c95075a
abid <fmaabid@gmail.com>
2018-10-17 12:36:09 +0600

    finishing the reducer for ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,14 +31,14 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount () {
-        console.log(this.props);
+        /*console.log(this.props);
         axios.get( 'https://react-my-burger.firebaseio.com/ingredients.json' )
             .then( response => {
                 this.setState( { ingredients: response.data } );
             } )
             .catch( error => {
                 this.setState( { error: true } );
-            } );
+            } );*/
     }
 
     updatePurchaseState ( ingredients ) {
